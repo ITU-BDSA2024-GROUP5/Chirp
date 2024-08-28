663c5c59f9cf27b47eade411ccb7ede3e32f8db8
Nicky Ye <niye@itu.dk>
2024-08-28 15:01:45 +0200

    Added read and cheep functionality
    Co-authored-by: Sara <salj@itu.dk>
    Co-authored-by: Patrick <pash@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿var path = "chirp_cli_db.csv";
+
+//https://learn.microsoft.com/en-us/dotnet/api/system.io.streamreader?view=net-8.0
+if (args[0] == "read")
+{
+    try
+    {
+        // Open the text file using a stream reader.
+        using StreamReader reader = new(path);
+
+        // Read the stream as a string.
+        reader.ReadLine();
+        string line;
+        // Read and display lines from the file until the end of
+        // the file is reached.
+        while ((line = reader.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+
+//https://learn.microsoft.com/en-us/dotnet/api/system.io.file.appendtext?view=net-7.0
+if (args[0] == "cheep")
+{
+    using (StreamWriter sw = File.AppendText(path))
+    {
+        DateTimeOffset utcTime = DateTimeOffset.UtcNow;
+        sw.WriteLine(Environment.UserName + "," + args[1] + "," + utcTime.ToUnixTimeSeconds());
+    }	
+}
