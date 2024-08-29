e7055a8914728da4e15e2da56e7f279d4d93f222
Markus Sværke Staael <msvs@itu.dk>
2024-08-29 14:03:18 +0200

    [Window Title]

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,33 +1,101 @@
-﻿var path = "chirp_cli_db.csv";
+﻿public class Program {
+    static String path = "chirp_cli_db.csv";
 
-//https://learn.microsoft.com/en-us/dotnet/api/system.io.streamreader?view=net-8.0
-if (args[0] == "read")
-{
-    try
+    public static void Main(string[] args)
     {
-        using (StreamReader reader = File.OpenText(path))
+        if (args.Length <= 0)
         {
-            reader.ReadLine();
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
+            // test for no argument
+            Console.WriteLine("No arguments provided, try 'help' for a list of commands.");
+            return;
+        }
+
+
+        switch (args[0]) // check first argument (command)
+        {
+            case "read":
+                Read.run();
+                break;
+            case "help":
+                Help.run();
+                break;
+            case "cheep":
+                Cheep.run(args);
+                break;
+            default:
+                Console.WriteLine(args[0] + " not recognized as a command, try 'help' for a list of commands.");
+                break;
         }
     }
-    catch (IOException e)
-    {
-        Console.WriteLine("The file could not be read:");
-        Console.WriteLine(e.Message);
-    }
-}
 
-//https://learn.microsoft.com/en-us/dotnet/api/system.io.file.appendtext?view=net-7.0
-if (args[0] == "cheep")
-{
-    using (StreamWriter sw = File.AppendText(path))
+
+    class Read
     {
-        DateTimeOffset utcTime = DateTimeOffset.UtcNow;
-        sw.WriteLine(Environment.UserName + "," + args[1] + "," + utcTime.ToUnixTimeSeconds());
-    }	
+        public static void run() {
+            try
+            {
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    reader.ReadLine();
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        public static String info()
+        {
+            return "'read' - Reads and prints current chirps.";
+        }
+        
+    } 
+
+    class Help
+    {
+        public static void run() {
+            Console.WriteLine("List of available commands:");
+            Console.WriteLine(Read.info());
+            Console.WriteLine(Cheep.info());
+            Console.WriteLine(Help.info());
+        }
+
+        public static String info()
+        {
+            return "'help' - Lists available commands.";
+        }
+    }
+    
+    class Cheep {
+        public static void run(String[] args) //https://learn.microsoft.com/en-us/dotnet/api/system.io.file.appendtext?view=net-7.0
+        {
+            if (args.Length <= 1)
+            {
+                Console.WriteLine("Not enough arguments provided to cheep.");
+                return; 
+            }
+            
+            
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                DateTimeOffset utcTime = DateTimeOffset.UtcNow;
+                sw.WriteLine(Environment.UserName + "," + args[1] + "," + utcTime.ToUnixTimeSeconds());
+                Console.WriteLine("Sucessfully cheeped '"+args[1]+"'");
+            }
+        }
+        
+        public static String info()
+        {
+            return "'cheep' - Cheeps your second argument.";
+        }
+      
+    }
 }
