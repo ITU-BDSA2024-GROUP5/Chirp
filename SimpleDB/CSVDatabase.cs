e1641034c67ff4311c6d70eb1f38b1f5653a23ec
Nicky Ye <niye@itu.dk>
2024-09-04 14:07:21 +0200

    Add class CSVDatabase that implements interface

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,13 @@
+namespace SimpleDB
+
+class CSVDatabase<T> implements IDatabaseRepository<T>{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        
+    }
+
+    public void Store(T record)
+    {
+        
+    }
+}
