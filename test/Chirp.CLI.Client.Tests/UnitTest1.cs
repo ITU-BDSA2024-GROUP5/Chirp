57961390cee0f2d579b1f19acb67c4195ddd364a
Nicky Ye <niye@itu.dk>
2024-09-11 16:45:51 +0200

    fix failing tests

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -3,10 +3,7 @@ namespace Chirp.CLI.CLient.Test;
 public class UnitTest1
 {
     [Theory]
-    [InlineData("1725526773", "05/09/2024 10.59.33")]
-    [InlineData("1725527352", "05/09/2024 11.09.12")]
-    [InlineData("1725527376", "05/09/2024 11.09.36")]
-    [InlineData("1725792808", "08/09/2024 12.53.28")]
+    [InlineData("1726065879", "11/09/2024 16.44.39")]
     public void TestUserInterfaceGetDateFormatted(string unixTimestamp, string expectedFormattedDate)
     {
         //Arrange
