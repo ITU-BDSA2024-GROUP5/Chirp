200f468ef4a9cb35424485dfa5008d7c47400822
Nicky Ye <niye@itu.dk>
2024-09-11 14:32:19 +0200

    Add test for UserInterface method

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -3,9 +3,16 @@ namespace Chirp.CLI.CLient.Test;
 public class UnitTest1
 {
     [Theory]
-    [InlineData()]
-    public void Test()
+    [InlineData("1725526773", "05/09/2024 10.59.33")]
+    [InlineData("1725527352", "05/09/2024 11.09.12")]
+    [InlineData("1725527376", "05/09/2024 11.09.36")]
+    [InlineData("1725792808", "08/09/2024 12.53.28")]
+    public void TestGetDateFormatted(string unixTimestamp, string expectedFormattedDate)
     {
-        
+        //Arrange
+        //Act
+        var actual = UserInterface.GetDateFormatted(unixTimestamp);
+        //Assert
+        Assert.Equal(actual, expectedFormattedDate);
     }
 }
