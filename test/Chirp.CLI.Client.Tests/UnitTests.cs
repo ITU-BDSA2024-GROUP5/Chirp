2915bce97836be0383e3fa8cc133d6d557b0a9a6
Nicky Ye <niye@itu.dk>
2024-09-16 11:37:33 +0200

    test test

--- test/Chirp.CLI.Client.Tests/UnitTests.cs
+++ test/Chirp.CLI.Client.Tests/UnitTests.cs
@@ -5,7 +5,7 @@ using SimpleDB;
 public class UnitTests
 {
     [Theory]
-    [InlineData("1726065879", "11/09/2024 16.44.39")]
+    [InlineData("1726065879", "11/09/2024 16:44:39")]
     public void TestUserInterfaceGetDateFormatted(string unixTimestamp, string expectedFormattedDate)
     {
         //Arrange aa
