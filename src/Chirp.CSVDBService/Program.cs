1df55946be35ee28676430885c2f08ce1521e7f0
Nicky Ye <niye@itu.dk>
2024-09-18 14:06:41 +0200

    create web service project

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/dav", () => "dav");
+app.MapGet("/cheeps", () => new Cheep("mæ", "hej", 1684229348));
+app.MapPost("/cheep", () => new Cheep("mæ2", "hej", 1684229348));
 
 app.Run();
+
+public record Cheep(string author, string messasge, long timestamp);
