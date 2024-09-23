e7d3ee8f2fdd6deec934f20ceafab818fc25bd9c
salj <salj@itu.dk>
2024-09-23 11:39:07 +0200

    introducing mock http client to end2end test, and fixing the issues
    Co-authored-by: Nicky Ye <niye@itu.dk>

--- test/Chirp.CLI.Client.Tests/EndToEndTests.cs
+++ test/Chirp.CLI.Client.Tests/EndToEndTests.cs
@@ -1,6 +1,11 @@
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
 namespace Chirp.Cli.Client.Test;
 
 using System.Diagnostics;
+using SoloX.CodeQuality.Test.Helpers.Http;
 using CsvHelper;
 using System.Globalization;
 using SimpleDB;
@@ -9,34 +14,31 @@ using System.IO;
 public class EndToEndTests
 {
     public record Cheep(string Author, string Message, string Timestamp);
+    
     [Fact] 
-    public void EndToEnd()
+    public async Task EndToEnd()
     {
-        // Arrange
+        
+        var mockCheep = new Cheep("nickyye", "hej", "11/09/2024 16.44.39");
+
         // Act
-        string output = "";
-        using (var process = new Process())
-        {
-            process.StartInfo.FileName = "../../../../../src/Chirp.CLI.Client/bin/Debug/net7.0/Chirp.CLI";
-            process.StartInfo.Arguments = "read";
-            process.StartInfo.UseShellExecute = false;
-            // process.StartInfo.WorkingDirectory = "";
-            process.StartInfo.RedirectStandardOutput = true;
-            process.Start();
-            // Synchronously read the standard output of the spawned process.
-            StreamReader reader = process.StandardOutput;
-            output = reader.ReadToEnd();
-            
-            
-            process.WaitForExit();
-        }
-        foreach(String tmp in output.Split("\n"))
-        {
-            Console.WriteLine(tmp);
-        }
-        string fstCheep = output.Split("\n")[0];
+        var mockHttpClient = new HttpClientMockBuilder()
+            .WithBaseAddress(new Uri("http://testwebsite"))
+            .WithRequest("/cheeps")
+            .RespondingJsonContent<List<Cheep>>(
+                request => new List<Cheep> { mockCheep }
+            )
+            .Build();
+
+        var response = await mockHttpClient.GetFromJsonAsync<List<Cheep>>("/cheeps");
+
+        Assert.NotNull(response); 
+        Assert.NotEmpty(response);
+
+        string expectedAuthorName = response[0].Author; // Get the first author's name
+        
         // Assert
-        Assert.StartsWith("nickyye", fstCheep);
-        Assert.EndsWith("test with csv append to file", fstCheep);
-    }
+        Assert.Equal("nickyye", expectedAuthorName);
+        
+    } 
 }
