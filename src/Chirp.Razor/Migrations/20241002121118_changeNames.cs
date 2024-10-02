93e15647d240b85d13326c9bd48472187d0ed642
salj <salj@itu.dk>
2024-10-02 14:13:41 +0200

    Track cheep.db
    Co-authored-by: Markus Sværke Staael <msvs@itu.dk>
    Co-authored-by: Patrick Shen <pash@itu.dk>
    Co-authored-by: Frederik Terp <fter@itu.dk>
    Co-authored-by: Nicky Ye <niye@itu.dk>

---
+++ src/Chirp.Razor/Migrations/20241002121118_changeNames.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cheeps",
+                columns: table => new
+                {
+                    CheepID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    AuthorID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cheeps", x => x.CheepID);
+                    table.ForeignKey(
+                        name: "FK_Cheeps_Authors_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cheeps_AuthorID",
+                table: "Cheeps",
+                column: "AuthorID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cheeps");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
