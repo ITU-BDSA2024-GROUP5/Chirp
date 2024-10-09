949b5be93e75a3074a794b516ce224c6ff4a271e
Nicky Ye <niye@itu.dk>
2024-10-09 15:19:39 +0200

    fix release file

--- src/Chirp.Razor/CheepModel.cs
+++ src/Chirp.Razor/CheepModel.cs
@@ -2,7 +2,7 @@ public class Cheep
 {
     public int CheepId{ get; set; }
     public int AuthorId { get; set; }
-    public string  Text{ get; set; }
+    public string Text{ get; set; }
     public DateTime TimeStamp{ get; set; }
     public Author Author{ get; set; }
 }
