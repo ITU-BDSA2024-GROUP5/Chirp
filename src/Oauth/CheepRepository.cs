7d1e9e50ef36e94840d2060ba81abada94bca06c
Nicky Ye <niye@itu.dk>
2024-10-29 12:42:40 +0100

    Ensure created applicationdb db
    Co-authored-by: mariuslarsen <coml@itu.dk>

--- src/Oauth/CheepRepository.cs
+++ src/Oauth/CheepRepository.cs
@@ -1,16 +1,21 @@
 ﻿using Oauth.Pages;
 
 using Microsoft.EntityFrameworkCore;
+using Oauth.Data;
 
 public class CheepRepository : ICheepRepository
 {
     private readonly CheepDBContext _context;
+    private readonly ApplicationDbContext _context2;
 
-    public CheepRepository(CheepDBContext context)
+    public CheepRepository(CheepDBContext context, ApplicationDbContext context2)
     {
         _context = context;
         context.Database.EnsureCreated();
         DbInitializer.SeedDatabase(_context);
+        
+        _context2 = context2;
+        context2.Database.EnsureCreated();
     }
 
     public async Task<List<CheepDTO>> Read(int page)
