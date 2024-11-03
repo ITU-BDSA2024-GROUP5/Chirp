c33b908fe5a8befe450fac6e47120093b05ad848
Nicky Ye <niye@itu.dk>
2024-11-03 16:52:54 +0100

    add cheep box to usertimeline also

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,24 +1,55 @@
-﻿using Chirp.Infrastructure.Data.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+using Chirp.Infrastructure.Data.DTO;
 using Chirp.Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SQLitePCL;
 
 namespace Chirp.Web.Pages;
 
 public class UserTimelineModel : PageModel 
 {
+    [BindProperty]
+    [Required]
+    [StringLength(160,ErrorMessage = "Maximum length is 160 characters.")]
+    public string Text { get; set; }
     public required List<CheepDTO> Cheeps { get; set; }
     
     private readonly ICheepRepository _cheepRepository;
     private readonly IAuthorRepository _authorRepository;
-    private readonly ICheepServiceDB _cheepServiceDB;
+    private readonly ICheepServiceDB _cheepServiceDb;
 
     public UserTimelineModel(ICheepRepository cheepRepository, IAuthorRepository authorRepository)
     {
         _authorRepository = authorRepository;
         _cheepRepository = cheepRepository;
-        _cheepServiceDB = new CheepServiceDB(cheepRepository, authorRepository);
+        _cheepServiceDb = new CheepServiceDB(cheepRepository, authorRepository);
+    }
+    
+    public async Task<ActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty, "you made an oopsie");
+            return Page();
+        }
+
+        var author = await _cheepServiceDb.GetAuthorByString(User.Identity.Name);
+        if (author == null)
+        {
+            var newAuthor = await _cheepServiceDb.CreateAuthor(User.Identity.Name);
+            author = newAuthor;
+        }
+        var cheep = await _cheepServiceDb.CreateCheep(author.Name, Text);
+        await _cheepServiceDb.WriteCheep(cheep);
+        
+        await fetchCheeps(author.Name);
+        
+        return RedirectToPage(author);
+    }
+    
+    public async Task fetchCheeps(string author)
+    {
+        Cheeps = await _cheepRepository.ReadByAuthor(0, author);
     }
     
     public async Task<ActionResult> OnGet(string author)
