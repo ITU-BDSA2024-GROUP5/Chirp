0d9956e420b2f7f86f66903b7bfbf1a6d2bfcb52
Nicky Ye <niye@itu.dk>
2024-11-04 21:35:10 +0100

    add cheeping test and usertimeline cheeps visible test

--- test/PlaywrightTests/End2endtests.cs
+++ test/PlaywrightTests/End2endtests.cs
@@ -3,6 +3,7 @@ using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
 namespace PlaywrightTests;
+
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
 public class Tests : PageTest
@@ -29,17 +30,17 @@ public class Tests : PageTest
     public async Task UserCanRegister()
     {
         await Page.GotoAsync("http://localhost:5177/");
-        
+
         await Page.GetByRole(AriaRole.Link, new() { Name = "Register" }).ClickAsync();
-        
+
         await Page.GetByText("Email").FillAsync("testuser@gmail.com");
         await Page.GetByText("Password", new() { Exact = true }).FillAsync("Nicepassword123#");
         await Page.GetByText("Confirm Password").FillAsync("Nicepassword123#");
-        
+
         await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-        
+
         await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
-        
+
         await Expect(Page.GetByText("Thank you for confirming your")).ToBeVisibleAsync();
     }
 
@@ -51,10 +52,10 @@ public class Tests : PageTest
         await Page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testuser@gmail.com");
         await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).FillAsync("Nicepassword123#");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-        
+
         await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "Hello testuser@gmail.com!" })).ToBeVisibleAsync();
     }
-    
+
     [Test]
     public async Task CheepBoxNotVisibleWhenNotLoggedIn()
     {
@@ -62,7 +63,7 @@ public class Tests : PageTest
         await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Share" })).Not.ToBeVisibleAsync();
 
     }
-    
+
     [Test]
     public async Task CheepBoxVisibleWhenLoggedIn()
     {
@@ -71,9 +72,35 @@ public class Tests : PageTest
         await Page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testuser@gmail.com");
         await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).FillAsync("Nicepassword123#");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-        
+
         await Page.GotoAsync("http://localhost:5177/Public");
         await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Share" })).ToBeVisibleAsync();
 
     }
+
+    [Test]
+    public async Task UserCanCheep()
+    {
+        await Page.GotoAsync("http://localhost:5177/");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Email" }).FillAsync("testuser@gmail.com");
+        await Page.GetByRole(AriaRole.Textbox, new() { Name = "Password" }).FillAsync("Nicepassword123#");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+
+        await Page.GotoAsync("http://localhost:5177/Public");
+        await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Share" })).ToBeVisibleAsync();
+
+        await Page.GetByRole(AriaRole.Textbox).FillAsync("This is a cheep");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+
+        await Expect(Page.GetByText("This is a cheep")).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task UserTimeLineHasCheeps()
+    {
+        await Page.GotoAsync("http://localhost:5177/testuser@gmail.com");
+        await Expect(Page.GetByText("There are no cheeps so far.")).Not.ToBeVisibleAsync();
+    }
+
 }
