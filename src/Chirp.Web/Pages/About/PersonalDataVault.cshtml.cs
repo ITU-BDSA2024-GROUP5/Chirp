3033e77bc1bbc900a6114e9ed03e4034c1540578
Marius Larsen <54457839+MessiGames30@users.noreply.github.com>
2024-11-20 11:18:25 +0100

    Now shows mail in personal data vault

--- src/Chirp.Web/Pages/About/PersonalDataVault.cshtml.cs
+++ src/Chirp.Web/Pages/About/PersonalDataVault.cshtml.cs
@@ -6,23 +6,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Build.Framework;
+using System.Collections.Generic;
 
 namespace Chirp.Web.Pages.About
 {
-    public class PersonalDataVaultModel : PageModel
+    public class PersonalDataVaultModel(ApplicationDbContext context,
+    UserManager<Author> userManager) : PageModel
     {
         private readonly ILogger<PersonalDataVaultModel> _logger;
 
         public string Username { get; private set; }
 
-        public PersonalDataVaultModel(ILogger<PersonalDataVaultModel> logger)
-        {
-            _logger = logger;
-        }
+        public List<PersonalDataItem> PersonalDataItems { get; private set; }
 
         public void OnGet(string username)
         {
             Username = username;
+            var user = userManager.FindByNameAsync(username).Result;
+            
+            PersonalDataItems = new List<PersonalDataItem>
+            {
+                new PersonalDataItem { Key = "Email", Value = user.Email },
+            };
         }
+        public class PersonalDataItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
     }
 }
